e685ddb03e908f29271dcc0c1b5f6b13890b11b2
“[Anna <[annaelida.rylander@gmail.com]>
2021-12-06 22:25:56 +0100

    day1

---
+++ advent1.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const internal = require("stream");
+
+let data = fs.readFileSync("input.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  return data;
+});
+
+numbers = data.split("\n");
+numbers = numbers.map((num) => Number(num));
+let count = 0;
+
+numbers.forEach(function (number, index) {
+  if (numbers[index] > numbers[index - 1]) {
+    //console.log(index, "increasing");
+    count += 1;
+  }
+});
+
+console.log(count);
