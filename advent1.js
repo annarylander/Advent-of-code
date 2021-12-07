d01189ee5d98f45e47873f0cd5982ccff3d3fb7d
“[Anna <[annaelida.rylander@gmail.com]>
2021-12-07 19:35:05 +0100

    part2

--- advent1.js
+++ advent1.js
@@ -11,13 +11,30 @@ let data = fs.readFileSync("input.txt", "utf8", (err, data) => {
 
 numbers = data.split("\n");
 numbers = numbers.map((num) => Number(num));
+
 let count = 0;
 
 numbers.forEach(function (number, index) {
   if (numbers[index] > numbers[index - 1]) {
-    //console.log(index, "increasing");
     count += 1;
   }
 });
 
 console.log(count);
+
+//Pt.2
+
+let currentSum = 0;
+let previousSum = 0;
+let countSum = 0;
+
+numbers.forEach(function (number, index) {
+  currentSum = numbers[index] + numbers[index + 1] + numbers[index + 2];
+  previousSum = numbers[index - 1] + numbers[index] + numbers[index + 1];
+
+  if (currentSum > previousSum) {
+    countSum += 1;
+  }
+});
+
+console.log(countSum);
